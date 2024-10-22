4ff87bb23ecabc075f2605634e0cc1ba59ab342b
laputa-n <skatmdgur6365@gmail.com>
2024-10-22 17:20:46 +0900

    [Admin]:seedling: 페이지 추가

---
+++ src/pages/Admin/Oneonone/index.tsx
@@ -0,0 +1,9 @@
+import '@/index.css';
+
+export function AdminOneononePage() {
+  return (
+    <div className='text-6xl'>
+      <div className='text-center'>Admin Oneonone!</div>
+    </div>
+  );
+}
