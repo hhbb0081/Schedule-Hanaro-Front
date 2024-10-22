b1f82ea3c1516ecce3b15662057880bf199b9117
teadmu <dharmavarsa@naver.com>
2024-10-22 14:44:34 +0900

    feat:어드민 메인페이지 추가

--- src/pages/Admin/Main/index.tsx
+++ src/pages/Admin/Main/index.tsx
@@ -1,7 +1,6 @@
-
 export function AdminMainPage() {
   return (
-    <div className='pt-10 text-6xl'>
+    <div className='text-6xl'>
       <div className='text-center'>AdminMain!</div>
     </div>
   );
