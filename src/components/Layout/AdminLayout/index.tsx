2a098b9e95df50d59700fe89010a40abb75124ee
laputa-n <skatmdgur6365@gmail.com>
2024-10-23 13:46:41 +0900

    [Admin]� Sidebar(Nav) 추가

--- src/components/Layout/AdminLayout/index.tsx
+++ src/components/Layout/AdminLayout/index.tsx
@@ -3,11 +3,19 @@
 import { Header } from '@/components/Admin/Header';
 import React from 'react';
 >>>>>>> 8bdf065 ([Admin]� Sidebar(Nav) 추가)
+<<<<<<< HEAD
+=======
+import { Header } from '@/components/Admin/Header';
+import React from 'react';
+>>>>>>> 8bdf065 ([Admin]� Sidebar(Nav) 추가)
 import { Outlet } from 'react-router-dom';
+import Sidebar from '@/components/Admin/Sidebar';
 import Sidebar from '@/components/Admin/Sidebar';
 
 function AdminLayout() {
+function AdminLayout() {
   return (
+<<<<<<< HEAD
 <<<<<<< HEAD
     <div>
       Header임
@@ -20,10 +28,18 @@ function AdminLayout() {
       <div style={{ paddingTop: '75px', flex: 1 }}>
 >>>>>>> 8bdf065 ([Admin]� Sidebar(Nav) 추가)
       <div className='flex-1 pb-0 pt-[4rem]'>
+=======
+    <div style={{ display: 'flex' }}>
+      <Sidebar />
+      <Header bankName='하나은행 성수역점 [11]' />
+      <div style={{ paddingTop: '75px', flex: 1 }}>
+>>>>>>> 8bdf065 ([Admin]� Sidebar(Nav) 추가)
         <Outlet />
       </div>
     </div>
   );
 }
+
+export default AdminLayout;
 
 export default AdminLayout;
