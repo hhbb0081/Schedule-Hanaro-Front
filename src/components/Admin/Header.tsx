f37e2ccab7db2bf0631fcfd3a26498062b9b6adb
teadmu <dharmavarsa@naver.com>
2024-10-23 17:46:52 +0900

    [Fix]🥚rem으로 변경

--- src/components/Admin/Header.tsx
+++ src/components/Admin/Header.tsx
@@ -5,7 +5,7 @@ type HeaderProps = {
 export function Header({ bankName }: HeaderProps) {
   return (
     <header className='fixed left-[225px] w-full py-5 pl-6 shadow-lg'>
-      <h1 className='text-xl font-bold'>{bankName}</h1>
+      <h1 className='text-[2.5rem] font-bold'>{bankName}</h1>
     </header>
   );
 }
