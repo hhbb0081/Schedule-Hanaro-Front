2e85b211fcb0bb827c07897661c0a1b8b835ae94
laputa-n <skatmdgur6365@gmail.com>
2024-10-23 17:23:28 +0900

    �[Feat]대기 현황 컴포넌트 추가

--- src/pages/Admin/Visit/index.tsx
+++ src/pages/Admin/Visit/index.tsx
@@ -1,5 +1,11 @@
+import WaitingNum from '@/components/Admin/WaitingNum';
 function VisitPage() {
-  return <div>방문상담 관리 페이지</div>;
+  return (
+    <>
+      <div>방문상담 관리 페이지</div>
+      <WaitingNum />
+    </>
+  );
 }
 
 export default VisitPage;
