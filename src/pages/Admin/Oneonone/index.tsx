c08f2e534f3c938886b02fd4d86b951dc20e7fd7
laputa-n <skatmdgur6365@gmail.com>
2024-10-23 13:46:41 +0900

    [Admin]� Sidebar(Nav) 추가

--- src/pages/Admin/Oneonone/index.tsx
+++
@@ -1,9 +0,0 @@
-import '@/index.css';
-
-export function AdminOneononePage() {
-  return (
-    <div className='text-6xl'>
-      <div className='text-center'>Admin Oneonone!</div>
-    </div>
-  );
-}
