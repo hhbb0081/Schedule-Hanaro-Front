81000669d73218b631863b9439236f7537c3e3d9
laputa-n <skatmdgur6365@gmail.com>
2024-10-23 13:46:41 +0900

    [Admin]� Sidebar(Nav) 추가

--- src/pages/Admin/Telephone/index.tsx
+++
@@ -1,9 +0,0 @@
-import '@/index.css';
-
-export function AdminTelephonePage() {
-  return (
-    <div className='text-6xl'>
-      <div className='text-center'>Admin Telephone!</div>
-    </div>
-  );
-}
