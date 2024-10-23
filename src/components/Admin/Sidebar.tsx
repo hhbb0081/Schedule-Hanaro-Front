e7479115d3fc25033759cfe898e56e1ddc643989
laputa-n <skatmdgur6365@gmail.com>
2024-10-23 17:23:28 +0900

    �[Feat]대기 현황 컴포넌트 추가

--- src/components/Admin/Sidebar.tsx
+++ src/components/Admin/Sidebar.tsx
@@ -1,17 +1,18 @@
 import { Link, useLocation } from 'react-router-dom';
 
 function Sidebar() {
+  const location = useLocation();
 
-    const location = useLocation();
-
-    const isActive = (path: string) => location.pathname === path;
+  const isActive = (path: string) => location.pathname === path;
   return (
     <div className='flex h-screen w-52 flex-col bg-gray-800 text-white'>
       <ul className='m-auto flex w-full list-none flex-col items-center space-y-2 p-0'>
         <li className={`w-full ${isActive('/admin') ? 'bg-gray-700' : ''}`}>
           <Link
             to='/admin'
-            className='block p-3 text-center text-white no-underline'
+            className={`block p-3 text-center no-underline ${
+              isActive('/admin') ? 'text-white' : 'text-gray-400'
+            }`}
           >
             방문상담 관리
           </Link>
@@ -21,7 +22,9 @@ function Sidebar() {
         >
           <Link
             to='/admin/inquiry'
-            className='block p-3 text-center text-white no-underline'
+            className={`block p-3 text-center no-underline ${
+              isActive('/admin/inquiry') ? 'text-white' : 'text-gray-400'
+            }`}
           >
             1:1 문의 관리
           </Link>
@@ -31,7 +34,9 @@ function Sidebar() {
         >
           <Link
             to='/admin/call'
-            className='block p-3 text-center text-white no-underline'
+            className={`block p-3 text-center no-underline ${
+              isActive('/admin/call') ? 'text-white' : 'text-gray-400'
+            }`}
           >
             전화 문의 관리
           </Link>
