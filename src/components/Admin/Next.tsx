680c5dd51209f6e878aa59461349973cd921b5eb
teadmu <dharmavarsa@naver.com>
2024-10-23 17:23:25 +0900

    [Feat]🥚대기 현황 컴포넌트 ,버튼 컴포넌트 추가

--- src/components/Admin/Next.tsx
+++ src/components/Admin/Next.tsx
@@ -2,7 +2,7 @@ import { Button } from '../ui/button';
 
 export default function Next() {
   return (
-    <div>
+    <div className='flex h-screen items-center justify-center'>
       <Button className='inline-flex h-[4.5rem] w-[12rem] items-center justify-center gap-2 rounded-[30rem] bg-main/80 px-[1.5rem] py-[1.5rem]'>
         <div className='text-[1.75rem] font-extrabold text-white'>다음으로</div>
         <div className='relative h-[1.5rem] w-[1.5rem]'></div>
