ae623e83a8086a2a1a60e32446e05c0220ce549e
laputa-n <skatmdgur6365@gmail.com>
2024-10-23 17:23:28 +0900

    �[Feat]대기 현황 컴포넌트 추가

--- src/pages/Admin/Visit/index.tsx
+++ src/pages/Admin/Visit/index.tsx
@@ -1,11 +1,13 @@
 import InfoCard from '@/components/Admin/Infocard';
 import Next from '../../../components/Admin/Next';
+import WaitingNum from '@/components/Admin/WaitingNum';
 
 function VisitPage() {
   return (
     <div className='mx-6 mt-6'>
       <InfoCard waitingCount={2} estimatedTime={15} todayVisitors={72} />
       <Next />
+      <WaitingNum />
     </div>
   );
 }
