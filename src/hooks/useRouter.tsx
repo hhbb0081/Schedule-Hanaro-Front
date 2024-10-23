d0babe2443e49d82746ff27a7c9e48e1d6f436e3
laputa-n <skatmdgur6365@gmail.com>
2024-10-23 13:46:41 +0900

    [Admin]� Sidebar(Nav) 추가

--- src/hooks/useRouter.tsx
+++ src/hooks/useRouter.tsx
@@ -1,6 +1,7 @@
 import { MapLayout, ReservationLayout } from '@/components/Layout';
 import { MainLayout } from '@/components/Layout/MainLayout';
 import { RegisterLayout } from '@/components/Layout/Register';
+import AdminLayout from '@/components/Layout/AdminLayout';
 import {
   BranchDetailPage,
   DirectionPage,
@@ -17,6 +18,9 @@ import {
   ReservationVisitPage,
 } from '@/pages';
 //수정 예정 ..
+import CallPage from '@/pages/Admin/Call';
+import InquiryPage from '@/pages/Admin/Inquiry';
+import VisitPage from '@/pages/Admin/Visit';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const useRouter = () =>
@@ -97,6 +101,15 @@ export const useRouter = () =>
           path: '/register/inquiry',
           element: <RegisterInquiryFormPage />,
         },
+      ],
+    },
+    {
+      path: '/admin',
+      element: <AdminLayout />,
+      children: [
+        { index: true, element: <VisitPage /> },
+        { path: '/admin/inquiry', element: <InquiryPage /> },
+        { path: '/admin/call', element: <CallPage /> },
       ],
     },
   ]);
