c031b754affc9fe0b75ebf0da3272ca9dd33bf24
teadmu <dharmavarsa@naver.com>
2024-10-24 17:41:00 +0900

    [Feat]🥚전화 문의 관리 페이지 추가

--- src/components/Admin/Next.tsx
+++ src/components/Admin/Next.tsx
@@ -4,6 +4,8 @@ export default function Next() {
   return (
     <div className='flex h-screen items-center justify-center'>
       <Button className='inline-flex h-[4.5rem] w-[12rem] items-center justify-center gap-2 rounded-[30rem] bg-main/80 px-[1.5rem] py-[1.5rem]'>
+    <div>
+      <Button className='inline-flex h-[4.5rem] w-[12rem] items-center justify-center gap-2 rounded-[30rem] bg-main/80 px-[1.5rem] py-[1.5rem]'>
         <div className='text-[1.75rem] font-extrabold text-white'>다음으로</div>
         <div className='relative h-[1.5rem] w-[1.5rem]'></div>
       </Button>
