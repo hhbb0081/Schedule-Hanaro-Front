20c07f5ce0f09a567c003da1aa56629c07f64d62
Min-swo <msms0226@g.skku.edu>
2024-10-24 15:37:05 +0900

    [Feat] 🍎 경로 안내 상세 정보 전역 상태 관리 및 표시

--- src/pages/Direction/topSheet.tsx
+++ src/pages/Direction/topSheet.tsx
@@ -1,33 +1,97 @@
-import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
+import { BRANCH_MOCK } from '@/mock/branch_mock';
+import {
+  arrivalTimeAtom,
+  bankIdAtom,
+  currentAddressAtom,
+  departureTimeAtom,
+  totalDistanceAtom,
+  totalTimeAtom,
+} from '@/stores';
+import { useAtom } from 'jotai';
+import { useEffect } from 'react';
+import { ReactComponent as GreenCircle } from '@/assets/icons/green-circle.svg';
+import { ReactComponent as RegularBar } from '@/assets/icons/regular-bar.svg';
+import { ReactComponent as Close } from '@/assets/icons/close.svg';
 
 export default function TopSheet() {
+  const [bankId] = useAtom(bankIdAtom);
+  const [currentAddress] = useAtom(currentAddressAtom);
+  const [departureTime, setDepartureTime] = useAtom(departureTimeAtom);
+  const [arrivalTime, setArrivalTime] = useAtom(arrivalTimeAtom);
+  const [totalTime] = useAtom(totalTimeAtom);
+  const [totalDistance] = useAtom(totalDistanceAtom);
+  const bankIdx = BRANCH_MOCK.findIndex((bank) => bank.id === bankId);
+
+  useEffect(() => {
+    setArrivalTime(new Date());
+
+    if (!arrivalTime) return;
+
+    const tmpDate = new Date(arrivalTime);
+    tmpDate.setSeconds(tmpDate.getSeconds() + totalTime);
+    console.log('🚀 ~ TopSheet ~ tmpDate:', tmpDate);
+    console.log('🚀 ~ TopSheet ~ totalTime:', totalTime);
+    setDepartureTime(tmpDate);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [totalTime]);
+
   return (
     <div className='fixed top-10 z-10 h-[12rem] w-[26rem] rounded-md bg-white px-6 py-4'>
       <div className='flex h-full flex-col justify-between'>
         <div className='flex h-2/3 justify-between'>
           <div className='flex gap-3'>
             <div className='flex flex-col justify-between font-bold'>
-              <div>14:03</div>
-              <div>14:25</div>
+              <div>
+                {arrivalTime
+                  ? `${arrivalTime.getHours().toString().padStart(2, '0')}:
+                  ${arrivalTime.getMinutes().toString().padStart(2, '0')}`
+                  : `00:00`}
+              </div>
+              <div>
+                {departureTime
+                  ? `${departureTime.getHours().toString().padStart(2, '0')}:
+                  ${departureTime.getMinutes().toString().padStart(2, '0')}`
+                  : `00:00`}
+              </div>
             </div>
-            <Separator orientation='vertical' />
+            <div className='flex flex-col items-center justify-center gap-1'>
+              <GreenCircle></GreenCircle>
+              <RegularBar></RegularBar>
+              <RegularBar></RegularBar>
+              <RegularBar></RegularBar>
+              <RegularBar></RegularBar>
+              <RegularBar></RegularBar>
+              <RegularBar></RegularBar>
+              <RegularBar></RegularBar>
+              <RegularBar></RegularBar>
+              <GreenCircle></GreenCircle>
+            </div>
           </div>
 
-          <div className='flex flex-col justify-between'>
-            <div className='text-gray-400'>출발지</div>
-            <div className='font-bold'>서울 성동구 아차산로 113</div>
+          <div className='flex flex-col justify-between gap-1 text-left'>
+            <div className='flex flex-col'>
+              <div className='text-xs text-gray-400'>출발지</div>
+              <div className='font-bold'>{currentAddress || ''}</div>
+            </div>
+
             <Separator />
-            <div className='text-gray-400'>도착지</div>
-            <div className='font-bold'>하나은행 성수역점</div>
+            <div className='flex flex-col'>
+              <div className='text-xs text-gray-400'>도착지</div>
+              <div className='font-bold'>
+                {bankIdx === -1 ? '' : BRANCH_MOCK[bankIdx].name}
+              </div>
+            </div>
           </div>
-          <Button>X</Button>
+          <Close></Close>
         </div>
         <div className='mx-10 flex items-center justify-between'>
           <div className='flex items-end gap-3'>
             <div>소요시간</div>
             <div className='flex items-end'>
-              <div className='text-2xl font-bold'>22</div>
+              <div className='text-3xl font-bold'>
+                {(totalTime / 60).toFixed(0)}
+              </div>
               <div>분</div>
             </div>
           </div>
@@ -35,8 +99,19 @@ export default function TopSheet() {
           <div className='flex items-end gap-3'>
             <div>거리</div>
             <div className='flex items-end'>
-              <div className='text-2xl font-bold'>1.5</div>
-              <div>km</div>
+              {totalDistance < 1000 ? (
+                <>
+                  <div className='text-3xl font-bold'>{totalDistance}</div>
+                  <div>m</div>
+                </>
+              ) : (
+                <>
+                  <div className='text-2xl font-bold'>
+                    {totalDistance / 1000}
+                  </div>
+                  <div>km</div>
+                </>
+              )}
             </div>
           </div>
         </div>
