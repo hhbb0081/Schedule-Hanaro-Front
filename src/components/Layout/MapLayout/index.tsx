c2b59105bd49643fac4f0efdadc44c32136874d5
Min-swo <msms0226@g.skku.edu>
2024-10-24 15:35:02 +0900

    [Feat] 🍎 Jotai 전역 상태 관리

--- src/components/Layout/MapLayout/index.tsx
+++ src/components/Layout/MapLayout/index.tsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom';
 
 export function MapLayout() {
   return (
-    <div className='App'>
+    <>
       <Outlet />
       <MapPage />
-    </div>
+    </>
   );
 }
