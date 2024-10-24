c901b91ef56b3cd44709118dd69404e8af7e53f7
teadmu <dharmavarsa@naver.com>
2024-10-24 17:41:00 +0900

    [Feat]🥚전화 문의 관리 페이지 추가

--- src/components/Layout/AdminLayout/index.tsx
+++ src/components/Layout/AdminLayout/index.tsx
@@ -32,8 +32,7 @@ function AdminLayout() {
     <div style={{ display: 'flex' }}>
       <Sidebar />
       <Header bankName='하나은행 성수역점 [11]' />
-      <div style={{ paddingTop: '75px', flex: 1 }}>
->>>>>>> 8bdf065 ([Admin]� Sidebar(Nav) 추가)
+      <div className='flex-1 pb-0 pt-[4rem]'>
         <Outlet />
       </div>
     </div>
