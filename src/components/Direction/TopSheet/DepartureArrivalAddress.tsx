7060a1815cdbdde4caa2cc01543f51750d677235
Min-swo <msms0226@g.skku.edu>
2024-10-25 11:17:13 +0900

    [Chore] 🍎길찾기 페이지 폴더 구조 변경

--- src/components/Direction/TopSheet/DepartureArrivalAddress.tsx
+++ src/components/Direction/TopSheet/DepartureArrivalAddress.tsx
@@ -1,12 +1,12 @@
 import { Separator } from '@/components/ui/separator';
 import { BRANCH_MOCK } from '@/mock/branch_mock';
-import { bankIdAtom, currentAddressAtom } from '@/stores';
+import { branchIdAtom, currentAddressAtom } from '@/stores';
 import { useAtom } from 'jotai';
 
 export default function DepartureArrivalAddress() {
   const [currentAddress] = useAtom(currentAddressAtom);
-  const [bankId] = useAtom(bankIdAtom);
-  const bankIdx = BRANCH_MOCK.findIndex((bank) => bank.id === bankId);
+  const [branchId] = useAtom(branchIdAtom);
+  const branchIdx = BRANCH_MOCK.findIndex((branch) => branch.id === branchId);
 
   return (
     <div className='flex flex-col justify-between gap-1 text-left'>
@@ -19,7 +19,7 @@ export default function DepartureArrivalAddress() {
       <div className='flex flex-col'>
         <div className='text-xs text-gray-400'>도착지</div>
         <div className='font-bold'>
-          {bankIdx === -1 ? '' : BRANCH_MOCK[bankIdx].name}
+          {branchIdx === -1 ? '' : BRANCH_MOCK[branchIdx].name}
         </div>
       </div>
     </div>
