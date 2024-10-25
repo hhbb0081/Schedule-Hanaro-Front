55c5793c0f9144d8edecf2accba485cf1b1a0b45
Min-swo <msms0226@g.skku.edu>
2024-10-25 11:17:54 +0900

    [Feat] 🍎 예약 확인/취소 모달 구현

--- src/components/Direction/BottomFloatingBox/index.tsx
+++ src/components/Direction/BottomFloatingBox/index.tsx
@@ -1,11 +1,11 @@
-import BankInfo from './BankInfo';
+import BranchInfo from './BranchInfo';
 import ReservationButton from './ReservationButton';
 
 export default function BottomSheet() {
   return (
     <div className='fixed bottom-10 z-10 h-[12rem] w-[26rem] rounded-xl bg-white p-8'>
       <div className='flex h-full flex-col justify-between'>
-        <BankInfo />
+        <BranchInfo />
         <ReservationButton />
       </div>
     </div>
