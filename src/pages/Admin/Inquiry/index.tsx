32cf86bf049cf1a7bacacf6296991606acef02f0
laputa-n <skatmdgur6365@gmail.com>
2024-10-25 13:27:23 +0900

    🌱 [Feat]
    대기상태 탭바 추가

--- src/pages/Admin/Inquiry/index.tsx
+++ src/pages/Admin/Inquiry/index.tsx
@@ -1,5 +1,13 @@
+import ReplyState from '@/components/Admin/ReplyState';
+
 function InquiryPage() {
-  return <div>1:1문의</div>;
+  return (
+    <>
+      <div className='flex items-start justify-center py-14'>
+        <ReplyState />
+      </div>
+    </>
+  );
 }
 
 export default InquiryPage;
