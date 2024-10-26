2c2c9d3c4caf7812567a0a8c3241a9ac01450284
Min-swo <msms0226@g.skku.edu>
2024-10-26 15:45:20 +0900

    [Feat] 🍎 길찾기 페이지 네브바 적용

--- src/components/Direction/BottomFloatingBox/index.tsx
+++ src/components/Direction/BottomFloatingBox/index.tsx
@@ -3,7 +3,7 @@ import ReservationButton from './ReservationButton';
 
 export default function BottomSheet() {
   return (
-    <div className='fixed bottom-10 z-10 h-[12rem] w-[26rem] rounded-xl bg-white p-8'>
+    <div className='fixed bottom-[7rem] z-10 h-[12rem] w-[26rem] rounded-xl bg-white p-8'>
       <div className='flex h-full flex-col justify-between'>
         <BranchInfo />
         <ReservationButton />
