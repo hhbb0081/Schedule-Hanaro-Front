33847872c3ca6f2fd0cf1dd3f4ba253f831ca462
laputa-n <skatmdgur6365@gmail.com>
2024-10-27 15:58:46 +0900

    [Feat]🌱1:1문의 페이지 컴포넌트 추가

--- src/components/Admin/Header.tsx
+++ src/components/Admin/Header.tsx
@@ -1,13 +1,13 @@
 type HeaderProps = {
   bankName: string;
-  badnkNum: number;
+  bankNum: number;
 };
 
-export function Header({ bankName, badnkNum }: HeaderProps) {
+export function Header({ bankName, bankNum }: HeaderProps) {
   return (
     <header className='fixed left-[13rem] z-50 w-full py-[0.5rem] pl-[1.5rem] text-left shadow-[0_4px_50px_10px_rgba(0,0,0,0.07)]'>
       <h1 className='text-[1.5rem] font-extrabold'>
-        {bankName}[{badnkNum}]
+        {bankName}[{bankNum}]
       </h1>
     </header>
   );
