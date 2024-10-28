21172ebe0b5c35a57cae3d8394c1a9daa6fd29ae
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-10-29 02:25:51 +0900

    [Feat] 🍀branch mock data type 지정

---
+++ src/types/branch.ts
@@ -0,0 +1,16 @@
+export type BranchInfo = {
+  address: string;
+  id: string;
+  tel: string;
+  name: string;
+  driving_directions: string;
+  position_x: string;
+  position_y: string;
+  business_hours: string;
+};
+
+export type BranchStateInfo = {
+  id: string;
+  waiting_number: string;
+  waiting_time: string;
+};
