2d5080728b53f5c2131aa6d2ccdac7ed7189ff7e
laputa-n <skatmdgur6365@gmail.com>
2024-10-29 15:00:53 +0900

    [Refactor]🌱 UI 수정

--- src/components/Admin/Inquiry/InquiryList.tsx
+++ src/components/Admin/Inquiry/InquiryList.tsx
@@ -82,9 +82,13 @@ function InquiryList({
         boxShadow: '0px 4px 20px 0px rgba(0, 0, 0, 0.10)',
       }}
     >
-      <div className='mb-4 flex items-center justify-between border-b pb-4'>
-        <h2 className='text-lg font-semibold text-gray-800'>
-          총 <span className='text-teal-600'>{filteredInquiries.length}</span>건
+      <div className='font-inter mb-4 flex items-center justify-between border-b pb-4 font-normal leading-normal'>
+        <h2 className='text-[1.25rem] font-bold text-gray-800'>
+          총{' '}
+          <span className='text-[1.5rem] font-extrabold text-teal-600'>
+            {filteredInquiries.length}
+          </span>
+          건
         </h2>
         <FilterAndSearch setActiveCategory={setActiveCategory} />
       </div>
@@ -93,17 +97,19 @@ function InquiryList({
         {filteredInquiries.map(
           ({ id, title, status, category, time }, index) => (
             <AccordionItem key={id} value={id}>
-              <div className='flex items-center justify-between py-4'>
+              <div className='font-inter flex items-center justify-between py-4 font-normal leading-normal'>
                 <div className='flex items-center space-x-2'>
-                  <span className='font-medium text-gray-700'>{index + 1}</span>
-                  <span className='font-semibold text-gray-800'>
+                  <span className='text-[1.25rem] font-bold text-gray-700'>
+                    {index + 1}
+                  </span>
+                  <span className='text-[1.375rem] font-bold text-gray-800'>
                     {title.length <= 15
                       ? title
                       : `${title.substring(0, 15)}...`}
                   </span>
                   <Badge
                     variant='lightSolid'
-                    className={`rounded-full px-2 py-0.5 text-xs font-semibold ${
+                    className={`rounded-full px-2 py-0.5 text-[0.875rem] font-normal ${
                       status === '답변대기'
                         ? 'bg-teal-50 text-teal-600'
                         : 'bg-gray-200 text-gray-600'
@@ -121,7 +127,7 @@ function InquiryList({
                   </span>
                 ) : (
                   <AccordionTrigger
-                    className='flex items-center text-sm text-gray-500'
+                    className='flex items-center text-[1rem] font-normal text-gray-500'
                     onClick={() =>
                       setExpandedItem(expandedItem === id ? null : id)
                     }
@@ -131,23 +137,25 @@ function InquiryList({
                 )}
               </div>
               <AccordionContent>
-                <div className='mt-2 rounded-md p-4 shadow-inner'>
+                <div className='font-inter mt-2 rounded-md p-4 leading-normal shadow-inner'>
                   <div className='mb-1 flex items-center justify-between'>
-                    <p className='font-semibold text-gray-800'>{title}</p>
+                    <p className='text-[1.375rem] font-bold text-gray-800'>
+                      {title}
+                    </p>
                     {status === '답변대기' && (
                       <Button
                         variant='default'
-                        className='h-[2.4375rem] w-[7.5rem] rounded-full bg-main px-4 py-1 text-sm text-white'
+                        className='h-[2.4375rem] w-[7.5rem] rounded-full bg-main px-4 py-1 text-[1rem] font-extrabold text-white'
                         onClick={() => navigate('/admin/inquiry/answerInput')}
                       >
                         답변하기
                       </Button>
                     )}
                   </div>
-                  <p className='mb-2 text-left text-[0.75rem] text-gray-400'>
+                  <p className='mb-2 text-left text-[1rem] font-medium text-gray-400'>
                     작성자 · {time} · {category}
                   </p>
-                  <p className='text-left text-sm leading-relaxed text-gray-700'>
+                  <p className='text-left text-[1.125rem] font-medium leading-normal text-gray-700'>
                     문의 내용에 대한 상세 설명이 여기에 표시됩니다.
                   </p>
                 </div>
