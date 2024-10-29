40e5df41f41c9d8a1f90933c961e15eaa4905b2c
laputa-n <skatmdgur6365@gmail.com>
2024-10-29 22:51:17 +0900

    [Refactor]🌱 디자인 리뷰 반영

--- src/components/Admin/Header.tsx
+++ src/components/Admin/Header.tsx
@@ -5,7 +5,7 @@ type HeaderProps = {
 
 export function Header({ bankName, bankNum }: HeaderProps) {
   return (
-    <header className='fixed left-[13rem] z-50 w-full py-[0.5rem] pl-[1.5rem] text-left shadow-[0_4px_50px_10px_rgba(0,0,0,0.07)]'>
+    <header className='fixed left-[14rem] z-50 w-full py-[1rem] pl-[1.5rem] text-left shadow-[0_4px_50px_10px_rgba(0,0,0,0.07)]'>
       <h1 className='text-[1.5rem] font-extrabold'>
         {bankName}[{bankNum}]
       </h1>
--- src/components/Admin/WaitingNum.tsx
+++ src/components/Admin/WaitingNum.tsx
@@ -6,20 +6,20 @@ function WaitingNumber() {
   return (
     <div className='flex w-full flex-col items-center rounded-lg bg-white pb-[4.25rem]'>
       {/* 레이블 */}
-      <div className='mb-4 flex w-full max-w-xl justify-between px-2 md:px-8'>
+      <div className='mb-0 flex w-full max-w-xl justify-between px-2 md:px-8'>
         <span className='text-sm text-gray-400 md:text-lg'>이전 대기번호</span>
-        <span className='pt-10 text-sm text-gray-400 md:text-lg'>
+        <span className='pt-8 text-sm text-gray-400 md:text-lg'>
           현재 대기번호
         </span>
         <span className='text-sm text-gray-400 md:text-lg'>다음 대기번호</span>
       </div>
 
       {/* 대기번호 */}
-      <div className='flex w-full max-w-xl items-end justify-between px-2 md:px-8'>
+      <div className='mb-2 flex w-full max-w-xl items-end justify-between px-2 pb-6 md:px-8'>
         <span className='text-4xl font-bold text-gray-600 md:text-6xl'>
           {previousNumber}
         </span>
-        <span className='relative top-10 text-6xl font-extrabold text-black md:text-8xl'>
+        <span className='relative top-16 text-6xl font-extrabold text-black md:text-8xl'>
           {currentNumber}
         </span>
         <span className='text-4xl font-bold text-gray-600 md:text-6xl'>
