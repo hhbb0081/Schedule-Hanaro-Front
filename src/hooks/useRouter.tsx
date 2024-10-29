6225a203ca91d6a9dcadfd2b0fc1333eff854eed
teadmu <dharmavarsa@naver.com>
2024-10-29 10:28:36 +0900

    [Fix]🥚오류 수정

--- src/hooks/useRouter.tsx
+++ src/hooks/useRouter.tsx
@@ -17,7 +17,6 @@ import {
   ReservationVisitPage,
 } from '@/pages';
 //수정 예정 ..
-import { AdminMainPage } from '@/pages/Admin/Main';
 import AdminLayout from '@/components/Layout/AdminLayout';
 import CallPage from '@/pages/Admin/Call';
 import InquiryPage from '@/pages/Admin/Inquiry';
