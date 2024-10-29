900e292a7b8939c8ed734f81ca5cf9606e2f31f1
ImHyungsuk <limhs8630@gmail.com>
2024-10-29 16:18:22 +0900

    [Feat] 🍺 방문 예약 내역 디자인 수정

--- src/pages/Reservation/Visit/index.tsx
+++ src/pages/Reservation/Visit/index.tsx
@@ -18,33 +18,34 @@ export function ReservationVisitPage() {
       </div>
       <hr />
       {branchState.map(({ id, name, waiting_number, waiting_time }) => (
-        <button
-          onClick={() => moveToDetail(id)}
-          key={id}
-          className='mt-[1.4375rem] w-full'
-        >
-          <div className='mx-2 flex justify-between'>
-            <div className='text-2xl font-bold text-[#464646]'>{name}</div>
-            <div className='text-3xl font-bold text-[#008485]/80'>128</div>
-          </div>
-          <div className='mx-2 mt-4 flex justify-between'>
-            <div className='text-lg font-medium text-[#666666]'>
-              현재 대기 인원
+        <div key={id}>
+          <button
+            onClick={() => moveToDetail(id)}
+            className='mt-[1.4375rem] w-full px-2'
+          >
+            <div className='mx-2 flex justify-between'>
+              <div className='text-2xl font-bold text-[#464646]'>{name}</div>
+              <div className='text-3xl font-bold text-[#008485]/80'>128</div>
             </div>
-            <div className='text-xl font-bold text-[#464646]'>
-              {waiting_number}명
+            <div className='mx-2 mt-4 flex justify-between'>
+              <div className='text-lg font-medium text-[#666666]'>
+                현재 대기 인원
+              </div>
+              <div className='text-xl font-bold text-[#464646]'>
+                {waiting_number}명
+              </div>
             </div>
-          </div>
-          <div className='mx-2 mt-4 flex justify-between'>
-            <div className='text-lg font-medium text-[#666666]'>
-              예상 대기 시간
+            <div className='mx-2 mt-4 flex justify-between'>
+              <div className='text-lg font-medium text-[#666666]'>
+                예상 대기 시간
+              </div>
+              <div className='text-xl font-bold text-[#464646]'>
+                {waiting_time}분
+              </div>
             </div>
-            <div className='text-xl font-bold text-[#464646]'>
-              {waiting_time}분
-            </div>
-          </div>
+          </button>
           <hr className='mt-[1.4375rem]' />
-        </button>
+        </div>
       ))}
     </div>
   );
