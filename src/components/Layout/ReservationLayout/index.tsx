919aa3ed5058213bfb9b1c1eb6bcd2b7a223b647
ImHyungsuk <limhs8630@gmail.com>
2024-10-30 15:11:12 +0900

    [Fix] 🍺 특정 상황에서 예약 내역 탭바가 작동하지 않는 버그 수정

--- src/components/Layout/ReservationLayout/index.tsx
+++ src/components/Layout/ReservationLayout/index.tsx
@@ -1,10 +1,11 @@
 import ReservationHeader from '@/components/Header/ReservationHeader';
 import Nav from '@/components/Nav/Nav';
-// import Tabs from '@/components/Tabs/Tabs';
 import { Outlet, useLocation } from 'react-router-dom';
 
 export function ReservationLayout() {
   const location = useLocation();
+  const tab = location.pathname.replace('/reservation/', '');
+  const tabLocation = tab === 'visit' ? 'visit' : 'call';
 
   const noLayoutPaths = [
     '/reservation/visit/',
@@ -21,7 +22,7 @@ export function ReservationLayout() {
 
   return (
     <>
-      <ReservationHeader />
+      <ReservationHeader tabLocation={tabLocation} key={tabLocation} />
       <div className='h-screen w-[90%] justify-self-center pt-[12.5rem]'>
         <Outlet />
       </div>
