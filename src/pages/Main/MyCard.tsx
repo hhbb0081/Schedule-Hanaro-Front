0b122f74621ca2cecfb3c739c9ac23a80cc94e7c
Min-swo <msms0226@g.skku.edu>
2024-10-30 13:56:19 +0900

    [Fix] 🍎 메인페이지 UI 버그 수정

--- src/pages/Main/MyCard.tsx
+++ src/pages/Main/MyCard.tsx
@@ -8,15 +8,16 @@ type Props = {
 
 export default function MyCard({ title, content, onClick }: Props) {
   return (
-    <div className='h-36 w-1/2 rounded-[0.625rem] px-[0.9375rem] py-[1rem] shadow-[4px_4px_20px_0px_rgba(0,0,0,0.20)] md:h-[6.75rem] md:px-[0.75rem] md:py-[0.7rem]'>
+    <div className='h-[7rem] w-1/2 rounded-[0.625rem] px-[0.9375rem] py-[0.85rem] shadow-[4px_4px_20px_0px_rgba(0,0,0,0.20)] md:h-[6.25rem] md:px-[0.75rem] md:py-[0.7rem]'>
       <div className='flex h-full w-full flex-col items-start justify-between'>
-        <div className='text-[1.25rem] font-[600] md:text-[0.9rem]'>
-          {title}
-        </div>
-        <div className='text-[0.9rem] font-[400] text-lightGrey md:text-[0.75rem]'>
+        <div className='text-[1rem] font-[600] md:text-sm'>{title}</div>
+        <div className='text-[0.8rem] font-[400] text-lightGrey md:text-xs'>
           {content}
         </div>
-        <Button onClick={onClick} className='h-9 md:h-8 md:text-[0.75rem]'>
+        <Button
+          onClick={onClick}
+          className='h-[1.85rem] text-[0.8rem] md:h-[1.7rem] md:text-[0.75rem]'
+        >
           예약하기
         </Button>
       </div>
--- src/pages/Main/RecommendCard.tsx
+++ src/pages/Main/RecommendCard.tsx
@@ -7,14 +7,14 @@ type Props = {
 
 export default function RecommendCard({ title, content }: Props) {
   return (
-    <div className='flex h-[4rem] w-full items-center justify-between rounded-[0.625rem] px-[0.9375rem] py-[1rem] shadow-[4px_4px_20px_0px_rgba(0,0,0,0.20)] md:h-[3rem]'>
+    <div className='flex h-[3rem] w-full items-center justify-between px-[0.9375rem] py-[1rem] md:h-[3rem]'>
       <div className='flex w-full items-center gap-4'>
-        <div className='text-[1.25rem] font-[600] md:text-[1rem]'>{title}</div>
-        <div className='text-[1.1rem] font-[400] text-lightGrey md:text-[0.9rem]'>
+        <div className='text-[1rem] font-[600] md:text-[1rem]'>{title}</div>
+        <div className='pt-[0.1rem] text-[0.9rem] font-[400] text-lightGrey md:text-[0.9rem]'>
           {content}
         </div>
       </div>
-      <Button className='h-7 w-[6rem] rounded-[50rem] md:h-5 md:w-[4rem] md:text-[0.75rem]'>
+      <Button className='h-[1.5rem] w-[5rem] rounded-[50rem] text-[0.8rem] md:h-5 md:w-[4rem] md:text-[0.75rem]'>
         추천
       </Button>
     </div>
