016b464763be8229b85d3fd5d183ccfaef18bee3
teadmu <dharmavarsa@naver.com>
2024-10-30 09:44:58 +0900

    [Feat]🥚다음순서 3D

--- src/hooks/useRouter.tsx
+++ src/hooks/useRouter.tsx
@@ -134,9 +134,16 @@ export const useRouter = () =>
       element: <AdminLayout />,
       children: [
         { index: true, element: <VisitPage /> },
-        { path: '/admin/inquiry', element: <InquiryPage /> },
+        {
+          path: '/admin/inquiry',
+          element: <InquiryPage />,
+        },
+        {
+          path: '/admin/inquiry/:id',
+          element: <AnswerDetail />,
+        },
         { path: '/admin/inquiry/answerInput', element: <AnswerInput /> },
-        { path: '/admin/inquiry/answerDetail', element: <AnswerDetail /> },
+        // { path: '/admin/inquiry/answerDetail', element: <AnswerDetail /> },
         { path: '/admin/call', element: <CallPage /> },
       ],
     },
