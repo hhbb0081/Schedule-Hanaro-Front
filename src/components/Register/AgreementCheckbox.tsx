0f588e6c6bcf88a027b9f4f13157663ff0742fdb
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-11-22 20:44:31 +0900

    [Fix] 🍀build 오류 해결

--- src/components/Register/AgreementCheckbox.tsx
+++ src/components/Register/AgreementCheckbox.tsx
@@ -1,6 +1,6 @@
-import { Dispatch, SetStateAction, useState } from 'react';
-import { Check } from 'lucide-react';
 import ModalFormPage from '@/pages/Register/Modal';
+import { Check } from 'lucide-react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 interface AgreementCheckboxProps {
   isChecked1: boolean;
@@ -20,11 +20,11 @@ export function AgreementCheckbox({
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
 
-  const handleAgree = () => {
-    setIsChecked1(true);
-    setIsChecked2(true);
-    handleCloseModal();
-  };
+  // const handleAgree = () => {
+  //   setIsChecked1(true);
+  //   setIsChecked2(true);
+  //   handleCloseModal();
+  // };
 
   return (
     <div className='flex items-center space-x-2 pb-2'>
@@ -46,7 +46,7 @@ export function AgreementCheckbox({
           isChecked2={isChecked2}
           setIsChecked1={setIsChecked1}
           setIsChecked2={setIsChecked2}
-          handleAgree={handleAgree}
+          // handleAgree={handleAgree}
           handleClose={handleCloseModal}
         />
       )}
