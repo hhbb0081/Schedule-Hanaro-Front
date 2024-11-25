8b2eab697c42f013b0665d13f1865272ac8d54d5
teadmu <dharmavarsa@naver.com>
2024-11-25 15:20:25 +0900

    [Refactor]🥚 빙글 리팩토링

--- src/pages/Admin/offline/index.tsx
+++ src/pages/Admin/offline/index.tsx
@@ -1,34 +1,29 @@
-// VisitPage.tsx
 import InfoCard from '@/components/Admin/Infocard';
 import Next from '../../../components/Admin/Next';
 import WaitingNumber from '@/components/Admin/WaitingNum';
 import { useState } from 'react';
 import CallInfoBox from '@/components/Admin/main/CallInfoBox';
 
+const INITIAL_NUMBERS = [952, 953, 954, 955, 956, 957, 958, 951];
+const ROTATE_ANGLE = 360 / INITIAL_NUMBERS.length;
+
 function VisitPage() {
   const [selectedIdx, setSelectedIdx] = useState(123);
-
-  const [numbers, setNumbers] = useState([
-    952, 953, 954, 955, 956, 957, 958, 951,
-  ]);
+  const [numbers, setNumbers] = useState(INITIAL_NUMBERS);
   const [angle, setAngle] = useState(0);
   const [displayNum, setDisplayNum] = useState([7, 0, 1]);
-
-  const rotateAngle = 360 / 8;
 
   const handleNext = () => {
     setNumbers((prevNumbers) => {
       if (prevNumbers.length === 0) return prevNumbers;
       const lastNumber = prevNumbers[prevNumbers.length - 1];
-      return [lastNumber, ...prevNumbers.slice(0, prevNumbers.length - 1)];
+      return [lastNumber, ...prevNumbers.slice(0, -1)].map((num) => num + 1);
     });
-    setNumbers((prevNumbers) => {
-      return prevNumbers.map((num) => num + 1);
-    });
-    setAngle((prev) => prev + rotateAngle);
 
-    setDisplayNum((prevNumbers) =>
-      prevNumbers.map((num) => (num + 1 > 7 ? 0 : num + 1))
+    setAngle((prevAngle) => prevAngle + ROTATE_ANGLE);
+
+    setDisplayNum((prevDisplay) =>
+      prevDisplay.map((num) => (num + 1 > 7 ? 0 : num + 1))
     );
   };
 
@@ -38,6 +33,7 @@ function VisitPage() {
         <div className='left-[15%] right-[15%]'>
           <Next onClick={handleNext} />
         </div>
+
         <div className='mb-[3rem] mt-[3rem]'>
           <WaitingNumber
             numbers={numbers}
@@ -45,8 +41,10 @@ function VisitPage() {
             displayNum={displayNum}
           />
         </div>
+
         <InfoCard waitingCount={2} estimatedTime={15} todayVisitors={72} />
       </div>
+
       <div className='w-full rounded-[.9375rem] shadow-[0_4px_20px_0_rgba(0,0,0,0.1)]'>
         <CallInfoBox
           selectedIdx={selectedIdx}
